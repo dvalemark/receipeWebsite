74d081a214a8fa6b391e14bc3d5984c6b255a9c5
Disa <dvalemark@gmail.com>
2018-09-26 13:13:55 +0200

    Adding code to collect json-data from ingredientlist

--- app.js
+++ app.js
@@ -6,4 +6,16 @@ const app = express();
 // from the www folder
 app.use(express.static('www'));
 // Start the web server on port 3000
+app.use(express.json({extended: false}));
+
 app.listen(3000,() => console.log('Listening on port 3000'));
+
+const IngredientData = require('./classes/ingredientData.class');
+
+let ingredientDataJson= require('./json/livsmedelsdata.json');
+
+ingredientDataJson = ingredientDataJson.map(obj => new IngredientData(obj));
+
+let Routes = require('./classes/routes.class');
+
+new Routes (app, ingredientDataJson);
