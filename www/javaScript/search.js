fd14d9b57e0fdda6b0edfd1b93bd344642d41cfc
Disa <dvalemark@gmail.com>
2018-09-30 13:03:57 +0200

    Search functionality enabled for input
    Searches can be made with button and clicking the autocomplete options!

--- www/javaScript/search.js
+++ www/javaScript/search.js
@@ -2,6 +2,7 @@ $('#search-button').click(()  =>{
     let searchValue = $('#search-value').val();
     getRecipes(searchValue);
     
+    
 })
 
 $('#search-value').keyup(function(){
@@ -24,20 +25,34 @@ function getRecipes(searchValue){
     $.get('http://localhost:3000/recipes/' + searchValue, (data) => {
         $('#search-result').empty();
         data.forEach(listRecipes);
-            
+        $('#resultAc').empty();
     });
 }
 function listRecipes(recipe) {
     $('#search-result').append(`<h4> <a class="searchResult" href="#" data-value="${recipe.name}"> ${recipe.name}</a> </h4>`/* , `<p> ${recipe.description}</p>` */);
 }
 function listAc(recipe){
-    $('#resultAc').append(`<li class="list-group-item"><p> ${recipe.name}</p> </li> `);
+    $('#resultAc').append(`<li  class="list-group-item" id="selectedRecipe">${recipe.name}</li> `);
 }
 
 $('.searchResult').click(()=>{
     let showRecipe = $(this).dataset.val();
     console.log(showRecipe);
 })
+
+$('#selectedRecipe').click(()=>{
+    let searchVal= $('#selected').val;
+    console.log(searchVal);
+})
+
+$( "#resultAc" ).click(function( event ) {
+    var target = $( event.target );
+    if ( target.is( "li" ) ) {
+      var searchVal = $(this).text();
+      $('#search-value').val(searchVal);
+      getRecipes(searchVal);
+    }
+  });
 
 /* $('.searchResult').click(function( ev ){
     ev.preventDefault();
