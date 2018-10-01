6bf5e770eaa283f55f34089bdb271fc4cb98d648
Disa <dvalemark@gmail.com>
2018-10-01 14:06:29 +0200

    Added url encoded function

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 // from the www folder
 app.use(express.static('www'));
 // Start the web server on port 3000
-app.use(express.json({extended: false}));
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended: true}));
+
 
 app.listen(3000,() => console.log('Listening on port 3000'));
 
