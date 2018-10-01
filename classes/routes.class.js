379df65a8522d8209be6e9adfce1571795cc7c41
Disa <dvalemark@gmail.com>
2018-10-01 14:08:04 +0200

    All routes except for recipes/:partofrecipename aren't working
    Trial code there but the problem is not solved

--- classes/routes.class.js
+++ classes/routes.class.js
@@ -1,35 +1,34 @@
 const Recipe = require('./recipe_handler.class');
 
-
 module.exports = class Routes {
 
-  constructor(app, ingredients){
+  constructor(app, ingredients) {
     this.app = app;
     this.ingredients = ingredients;
     this.setRoutes();
   }
 
-  setRoutes(){
-//how to search through ingredients
-   /*  this.app.get(
-      '/autocomplete-ingredient-name/:startOfName',
-      (req, res) => {
-        
-        let start = req.params.startOfName.toLowerCase();
-        
-        if(start.length < 2){
-          res.json({error: 'Please provide at least two characters...'});
-          return;
-        }
-        
-        let result = this.ingredients.filter(
-          ingredient => ingredient.Namn.toLowerCase().indexOf(start) == 0
-        ).map(
-          ingredient => ingredient.Namn
-        );
-        res.json(result);
-      }
-    ); */
+  setRoutes() {
+    //how to search through ingredients
+    /*  this.app.get(
+       '/autocomplete-ingredient-name/:startOfName',
+       (req, res) => {
+         
+         let start = req.params.startOfName.toLowerCase();
+         
+         if(start.length < 2){
+           res.json({error: 'Please provide at least two characters...'});
+           return;
+         }
+         
+         let result = this.ingredients.filter(
+           ingredient => ingredient.Namn.toLowerCase().indexOf(start) == 0
+         ).map(
+           ingredient => ingredient.Namn
+         );
+         res.json(result);
+       }
+     ); */
     /* this.app.get(
       '/autocomplete-recipe-name/:startOfName',
       (req, res) => {
@@ -49,52 +48,73 @@ module.exports = class Routes {
       }
     ); */
 
-        this.app.get(
-          '/recipes/:partOfRecipeName',
-          async (req, res) => {
-            let value = req.params.partOfRecipeName.toLowerCase();
-    
-            if (value.length < 2) {
-              res.json({ error: 'Please provide at least two characters...' });
-              return;
-            }
-    
-            let recipes = require('../json/recipe.json') || [];
-            
-            recipes = recipes.filter((recipe) =>{
-              return recipe.name.toLowerCase().includes(value)
-            })
-    
-            res.json(recipes);
-          }
-        );
+    this.app.get('/recipeslist', (req, res) => {
+      console.log("i made it! I am empty");
+      res.json({ error: 'Please provide at least two characters...' });
+    });
 
-        this.app.get(
-          '/recipes/:category',
-          async (req, res) =>{
-            let valCategory = req.params.category.toLowerCase();
-            let categories = require('../json/recipe.json') || [];
-            
-            categories = categories.filter((recipe) =>{
-              if(recipe.category.toLowerCase().includes(valCategory)){
-              return recipe.name.toLowerCase();
-              }
-            })
-            res.json(recipe)
-          }
-        );
-      
-
-    /* this.app.get(
-      '/recipe-by-name/:name', 
+    this.app.get(
+      '/recipes/:partOfRecipeName',
       async (req, res) => {
-        let recipe = await Recipe.readFromFile(req.params.name)
-        res.json(recipe);
+        let value = req.params.partOfRecipeName.toLowerCase();
+
+        if (value.length < 2) {
+          res.json({ error: 'Please provide at least two characters...' });
+          return;
+        }
+
+        let recipes = require('../json/recipe.json') || [];
+
+        recipes = recipes.filter((recipe) => {
+          return recipe.name.toLowerCase().includes(value)
+        })
+
+        res.json(recipes);
       }
-    ); */
+    );
+
+    this.app.get(
+      '/recipes/:category',
+      async (req, res) => {
+        let valCategory = req.params.category.toLowerCase();
+        let categories = require('../json/recipe.json') || [];
+
+        categories = categories.filter((recipe) => {
+          if (recipe.category.toLowerCase().includes(valCategory)) {
+            return recipe.name.toLowerCase();
+          }
+        })
+        res.json(recipe)
+      }
+    );
+
+
+    this.app.get(
+    '/recipeslist/:name',
+    
+       async (req, res) => {
+        
+        let value = req.params.name.toLowerCase();
+        console.log("backend value ", value)
+        if (value.length === 0) {
+          res.json({ error: 'No recipe name' });
+          return;
+        }
+        let recipes = require('../json/recipe.json');
+        console.log("before find", recipes);
+
+        let recipe = recipes.find((recipe) => recipe.name.toLowerCase().includes(value));
+        console.log("after find", recipe);
+
+        res.json(recipe);
+      } 
+    );
+
+    
+
 
     this.app.post(
-      '/recipe', 
+      '/recipe',
       async (req, res) => {
         let recipe = new Recipe(req.body);
         let result = await recipe.writeToFile();
